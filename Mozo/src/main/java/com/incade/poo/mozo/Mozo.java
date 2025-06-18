060efe7bf056c8ad243d5ccfed72a60ab192c89c
Thiago Adorno <thiagoadorno06@gmail.com>
2025-06-18 16:55:20 -0300

    ADD user interface

--- Mozo/src/main/java/com/incade/poo/mozo/Mozo.java
+++ Mozo/src/main/java/com/incade/poo/mozo/Mozo.java
@@ -1,10 +1,14 @@
 package com.incade.poo.mozo;
 
 import com.incade.poo.mozo.repository.PersistenceController;
+import com.incade.poo.mozo.view.Ventana1;
 
 public class Mozo {
 
     public static void main(String[] args) {
         PersistenceController persistenceController = new PersistenceController();
+        Ventana1 v1 = new Ventana1();
+            v1.setVisible(true);
+            v1.setLocationRelativeTo(null);
     }
 }
